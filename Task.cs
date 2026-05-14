0478b3120dabb2a1d57b4bb096fd051388b6380d
Oleksandr Kravchenko <alaxandr.kravchenko@gmail.com>
2026-05-14 16:33:58 +0300

    complete user + add comment struct and methods in Task.cs

--- Task.cs
+++ Task.cs
@@ -2,4 +2,12 @@
 
 public class Task: WorkBase
 {
+    // Список коментарів до завдання
+    public List<Comment> Comments = new();
+    // Структура коментаря
+    public struct Comment
+    {
+        public string Message;
+        public User Author;
+    }
 }
--- User.cs
+++ User.cs
@@ -2,7 +2,35 @@
 
 public abstract class User
 {
-	public User()
+	// Характеристики користувача
+	protected string name;
+	protected string id;
+	protected bool isLoggedIn = false;
+
+    // Метод для створення коментаря до завдання (список і структура в Task.cs)
+    public void CreateComment(string message, Task task)
 	{
-	}
+		task.Comments.Add(new Task.Comment { Message = message, Author = this });
+    }
+
+    // Метод для авторизації користувача
+    public void LogIn(string name, string id)
+	{
+		if (this.id == id && this.name == name)
+			isLoggedIn = true;
+		else
+			throw new Exception("Invalid credentials");
+    }
+
+    // Метод для виходу користувача
+    public void LogOut()
+	{
+		isLoggedIn = false;
+    }
+
+	// Метод буде наслідуватись і доповнюватись
+	public void ShowInfo()
+	{
+		Console.Write($"Ім'я: {this.name}, ID: {this.id}");
+    }
 }
